4a4bf3cb44445915c0ca849b6beeeb7d0cfce413
Timelordregeneration10 <3497049745@qq.com>
2024-09-29 09:59:30 +0800

    cookies down test

--- app/utils/serverGetUserInfo.ts
+++ app/utils/serverGetUserInfo.ts
@@ -3,12 +3,12 @@ import { cookies } from "next/headers";
 import { UserInfo } from "../interfaces/user";
 
 export async function serverGetUserInfo(): Promise<UserInfo | null> {
-  const cookie = cookies();
-
-  let userInfoValue = cookie.get("cvwebuserInfo")?.value;
+  
   if (process.env.NEXT_PUBLIC_TEST === "test") {
     return { userId: "test" };
   }
+  const cookie = cookies();
+  let userInfoValue = cookie.get("cvwebuserInfo")?.value;
 
   if (!userInfoValue) {
     return null;
