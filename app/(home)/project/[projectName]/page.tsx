c056e51d6d26ef94309c9699d649edc04ab19466
Timelordregeneration10 <3497049745@qq.com>
2024-09-29 09:52:36 +0800

    fix: generateStaticParams

--- app/(home)/project/[projectName]/page.tsx
+++ app/(home)/project/[projectName]/page.tsx
@@ -1,12 +1,26 @@
+import { testProjectList } from "@/app/constants/testProjectList";
 import MainPage from "./components/mainPage";
 import ColorMapLayout from "./context/ColorMapContext";
 import RootDirLayout from "./context/RootDirContext";
+import API from "@/app/utils/api";
 
-// TODO
-
-export const dynamicParams = false;
 export async function generateStaticParams() {
-  return [];
+  const getProjectList = async () => {
+    if (process.env.NEXT_PUBLIC_TEST === "test") {
+      return testProjectList;
+    } else {
+      const projectList = API.getProjectList().then((res) => {
+        return res.data.data.project_list;
+      });
+      return projectList;
+    }
+  };
+
+  const projectList = await getProjectList();
+
+  return projectList.map((projectName: any) => ({
+    projectName: projectName,
+  }));
 }
 
 export default async function Page({
